cf8c612503021a9391e0877ab00529d8b7d9dc6b
izi2bhappy <israelkariti@gmail.com>
2022-12-17 16:48:21 +0200

    Btn making box open and close

--- Assets/Scripts/VariableButton.cs
+++ Assets/Scripts/VariableButton.cs
@@ -4,8 +4,25 @@ using UnityEngine;
 
 public class VariableButton :MonoBehaviour, Pressable
 {
+    public GameObject closedBox;
+    public GameObject openBox;
+
+    private bool isOpen = false;
+    private bool isClosed = true;
+
     public void OnPressed()
     {
         Debug.Log("pressed!");
+
+        isOpen = !isOpen;
+
+        // 
+        isClosed = !isClosed;
+
+        Debug.Log("isClosed after flip: " + isClosed);
+
+        closedBox.gameObject.SetActive(isClosed);
+        openBox.gameObject.SetActive(isOpen);
+
     }
 }
