6c7de7cb4eeffef7e91b59f6d0fb6aff988a1576
izi2bhappy <israelkariti@gmail.com>
2022-12-24 19:31:34 +0200

    Mega asterisk open box

---
+++ Assets/Scripts/MegaAsteriskLandingArea.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class MegaAsteriskLandingArea : MonoBehaviour, ILandingArea
+{
+    string currAddress;
+    private void Start()
+    {
+        // get the address of this box
+        currAddress = transform.parent.Find("Ticket/Text (TMP)").GetComponent<TMP_Text>().text;
+    }
+    public bool OnDraggableReleased(GameObject go)
+    {
+        MegaAsteriskOperator megaAsteriskOperator = go.GetComponent<MegaAsteriskOperator>();
+        if (megaAsteriskOperator != null)
+        {
+            //get the address on the mega asterisk
+            string megaAsteriskText = ExtractTextFromMegaAsterisk(go);
+            if (currAddress.Equals(megaAsteriskText))
+                return true;
+            else
+                return false;
+        
+        }
+        else
+            return false;
+    }
+
+    public void OnLandingArea(GameObject go)
+    {
+        // check if the address match
+        
+        // open the box
+        transform.parent.Find("BoxOpen").gameObject.SetActive(true);
+        transform.parent.Find("BoxClosed").gameObject.SetActive(false);
+        
+        Destroy(go);
+    }
+
+    private string ExtractTextFromMegaAsterisk(GameObject go)
+    {
+        // get the ticket
+        Transform ticketTransform = go.transform.Find("Ticket(Clone)");
+
+        // get the address from the ticket
+        Transform ticketTextTransform = ticketTransform.Find("Text (TMP)");
+
+        string ticketText = ticketTextTransform.GetComponent<TMP_Text>().text;
+
+        return ticketText;
+    }
+
+}
